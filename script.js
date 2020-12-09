4ff2e43d6a501bcf423c4e42ccdf351afdfd2fc6
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-12-09 14:24:48 -0400

    checkOrder and click

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ let score = 0;
 // 2 - amarelo
 // 3 - azul
 
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
-const green = document.querySelector('.green');
-const yellow = document.querySelector('.yellow');
+const blue = document.querySelector(".blue");
+const red = document.querySelector(".red");
+const green = document.querySelector(".green");
+const yellow = document.querySelector(".yellow");
 
+//cria ordem aleatória de cores
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
 
@@ -21,17 +22,44 @@ let shuffleOrder = () => {
   for (let i in order) {
     let elementColor = createColorElement(order[i]);
     lightColor(elementColor, Number(i) + 1);
-    
   }
-}
+};
 
+//acende a próxima cor
 let lightColor = (element, number) => {
   let time = number * 500;
   setTimeout(() => {
-    element.classList.add('selected');
+    element.classList.add("selected");
   }, time - 250);
 
   setTimeout(() => {
-    element.classList.remove('selected');
+    element.classList.remove("selected");
+  });
+};
+
+//che se os botoes clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+  for (let i in clickedOrder) {
+    if (clickedOrder[i] !== order[i]) {
+      lose();
+      break;
+    }
+  }
+
+  if (clickedOrder.length === order.length) {
+    alert(`Pontuação: ${score}\nVocê acertou! Iniciando próxima nível`);
+    nextLevel();
+  }
+};
+
+//função para o clique do usuario
+let click = (color) => {
+  clickedOrder[clickedOrder.length] = color;
+  createColorElement(color).classList.add('selected');
+
+  setTimeout(() => {
+    createColorElement(color).classList.remove('selected');
   })
-}
+
+  checkOrder();
+};
